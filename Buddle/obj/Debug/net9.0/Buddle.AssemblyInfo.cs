14248cb55967fffb56ba4f3b392a13ff2491184a
Hanzel Bernante <hanzel.bernante@wizard-ai.com>
2025-01-14 09:10:46 +0800

    Enlarge UI for all pages

--- Buddle/obj/Debug/net9.0/Buddle.AssemblyInfo.cs
+++ Buddle/obj/Debug/net9.0/Buddle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Buddle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4c3c31eb299bf7157ec1ecb902eda24f409efb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85bc6fefa2e022bd0bb38f7cee909b3a8c18037e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Buddle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Buddle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
